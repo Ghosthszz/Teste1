c37afc4fa5383c969e086bd7e7baa723fea0c6f5
Ghosthszz <142137414+Ghosthszz@users.noreply.github.com>
2023-11-08 14:14:12 -0300

    Atualizar o usuários.js

--- usuários.js
+++ usuários.js
@@ -1,12 +1,12 @@
-//usuários.js
-
-const  usuários  =  [
-    {  usuário : "usuário1" ,  senha : "senha123"  } ,
-    {  usuário : "usuario2" ,  senha : "senha456"  } ,
-    {  usuário : "sea@gmail.com" ,  senha : "Teste123"  } ,
+const usuários = [
+    { usuário: "usuário1", senha: "senha123" },
+    { usuário: "usuario2", senha: "senha456" },
+    { usuário: "sea@gmail.com", senha: "Teste123" },
+    { usuário: "design.sea@gmail.com", senha: "subsarino" },
+    { usuário: "Thiagoanjos08", senha: "123Mudar" }
     // Adicione mais usuários conforme necessário
-] ;
+];
 
-function  verificarCredenciais ( usuário ,  senha )  {
-    retornar  usuários . find ( u  =>  u . usuário  ===  usuário  &&  u . senha  ===  senha ) ;
+function verificarCredenciais(usuario, senha) {
+    return usuários.find(u => u.usuário === usuario && u.senha === senha);
 }
