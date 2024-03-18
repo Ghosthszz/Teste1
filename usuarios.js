724ab2561cf055fe08e5b117c46529e8287e9c19
Gustavo <142137414+Ghosthszz@users.noreply.github.com>
2024-03-18 17:54:10 -0300

    Update and rename users.js to usuarios.js

---
+++ usuarios.js
@@ -0,0 +1,8 @@
+{
+    "usuarios": [
+      { "usuario": "usuario1", "senha": "senha1" },
+      { "usuario": "usuario2", "senha": "senha2" },
+      { "usuario": "usuario3", "senha": "senha3" }
+    ]
+  }
+  
